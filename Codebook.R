ce33049d53e77ab32080afbe894b725b2f5d8db2
dugaldfoster <dugaldwefoster@gmail.com>
2018-09-05 13:59:45 +0100

    Add files via upload
    Study codebook containing all variables used in the study, along with their descriptions

--- Codebook.R
+++ Codebook.R
@@ -1,6 +1,6 @@
 Variable	Definition	
 
-caseid 	“Case identification used to uniquely identify each respondent”
+caseid “Case identification used to uniquely identify each respondent”
 v000 	“Alphabetic country code to identify the survey from which the data were collected”
 v001 	“Cluster number is the number identifying the sample point as used during the fieldwork”
 v002 	“Household number is the number identifying the household in which the respondent was interviewed”
@@ -13,14 +13,14 @@ v136 	“Total number of household members”
 v138 	“Number of eligible women in the household (eligible women are usually defined to be women aged 15-49 who slept in the household the previous night)”
 v149	“Educational achievement (0 = No education, 1 = Incomplete primary, 2 = Complete primary, 3 = Incomplete secondary, 4 = Complete secondary, 5 = Higher, 9 = Missing)”
 v150 	“Relationship to the head of the household”
-v190  “Wealth index”
+v190   “Wealth index”
 v191 	“Wealth index factor score (5 decimals)”
 v201 	“Total number of children ever born”
-v202  “Total number of sons living at home”
+v202   “Total number of sons living at home”
 v203 	“Total number of daughters living at home”
 v206 	“Total number of sons who have died”
 v207 	“Total number of daughters who have died”
-v212  “Age of the respondent at first birth”
+v212   “Age of the respondent at first birth”
 v218 	“Total number of living children”
 bidx 	“Birth history index numbers the entries in the birth history from 1 to n, where the nth birth is the first birth.”
 b0 	“Twin code gives an order number for each child of a multiple birth. Code 0 indicates a single birth, code 1-upwards give the number of the child.”
@@ -28,7 +28,7 @@ b2 	“Year of birth of child”
 b4 	“Sex of child (1 = Male, 2 = Female)”
 b5 	“Whether child was alive at the time of interview (0 = No, 1 = Yes)”
 b7	“Age at death (months)”
-b11 “Preceding birth interval is calculated as the difference in months between the current birth and the previous birth”
+b11   “Preceding birth interval is calculated as the difference in months between the current birth and the previous birth”
 b12 	“Succeeding birth interval is calculated as the difference in months between the current birth and the following birth”
 
 Computed Variables
@@ -37,7 +37,7 @@ tcd 		“Total number of children who have died”
 matage 	“Maternal age at birth x”
 overlap	“Whether the birth was an instance of reproductive overlap (0 = No, 1	= yes)”
 overlap	“Whether the birth was an instance of reproductive overlap between a mother and a daughter (0 = No, 1 = yes)”
-multimum “Whether the birth occurred in a household with >1 mother (0 = No, 1 = yes)”
+multimum   “Whether the birth occurred in a household with >1 mother (0 = No, 1 = yes)”
 
 For any other variables or codes, please see the DHS recode manuals: https://dhsprogram.com/publications/publication-dhsg4-dhs-questionnaires-and-manuals.cfm
 
